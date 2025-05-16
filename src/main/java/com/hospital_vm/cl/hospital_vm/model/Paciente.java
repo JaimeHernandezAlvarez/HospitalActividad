7846ef06343f3ef31c8621b51a4b1fb2f7b0bacb
Jaime <jai.hernandeza@duocuc.cl>
2025-05-16 14:47:16 -0400

    Something idk

---
+++ src/main/java/com/hospital_vm/cl/hospital_vm/model/Paciente.java
@@ -0,0 +1,38 @@
+package com.hospital_vm.cl.hospital_vm.model;
+
+import java.sql.Date;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="paciente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 13, nullable = false)
+    private String run;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = true)
+    private Date fecha_nacimiento;
+
+    @Column(nullable = false)
+    private String correo;
+}
